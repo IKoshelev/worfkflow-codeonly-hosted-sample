804b6838a3c90225ccdcc4571c67ad29bd15f424
IKoshelev <koshelew@live.com>
2018-05-18 23:49:43 +0200

    Got to 'is not valid activity type' message

---
+++ WorkflowService1/ExperimentalWorkflow.cs
@@ -0,0 +1,62 @@
+﻿using System.Activities;
+using System.Activities.Statements;
+using System.ServiceModel.Activities;
+using System.Xml.Linq;
+
+namespace WorkflowService1
+{
+    public class ExperimentalWorkflow : WorkflowService
+    {
+        public ExperimentalWorkflow()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.ConfigurationName = "ExperimentalWorkflow";
+
+            var requestVar = new Variable<int>() { Name = "request" };
+
+            this.Body = new Sequence()
+            {
+                Variables =
+                {
+                  requestVar
+                },
+                Activities =
+                {
+                  new Receive()
+                  {
+                    DisplayName = "ExperimentalWorkflow_Receive",
+                    OperationName = "Receive",
+                    ServiceContractName =
+                      (XName)
+                      "{http://www.acme.com/}IExperimentalWorkflow",
+                    Action =
+                      "http://www.acme.com/IExperimentalWorkflow/Receive",
+                    CanCreateInstance = true,
+                    Content = new ReceiveParametersContent()
+                    {
+                      Parameters =
+                      {
+                        { "Request", new OutArgument<int>(requestVar) }
+                      }
+                    }
+                  },
+                  new SendReply()
+                  {
+                    DisplayName = "ExperimentalWorkflow_SendReply",
+                    Content = new SendParametersContent()
+                    {
+                        Parameters =
+                        {
+                            { "Response", new InArgument<int>(requestVar) }
+                        }
+                    }
+                  }
+                }
+            };
+        }
+    }
+}
---
+++ WorkflowService1/IExperimentalWorkflow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace WorkflowService1
+{
+    [ServiceContract(Name = "IExperimentalWorkflow")]
+    public interface IExperimentalWorkflow
+    {
+        [OperationContract(Name = "Receive")]
+        int Receive(int requestVar);
+
+    }
+}
