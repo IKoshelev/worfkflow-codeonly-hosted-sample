69793452e49ed35a011f420d4e9c7c9b88dd2e95
IKoshelev <koshelew@live.com>
2018-05-19 19:02:49 +0200

    Great success

--- WorkflowService1/ExperimentalWorkflow.cs
+++ WorkflowService1/ExperimentalWorkflow.cs
@@ -1,62 +1,71 @@
-﻿using System.Activities;
+﻿using System;
+using System.Activities;
 using System.Activities.Statements;
 using System.ServiceModel.Activities;
 using System.Xml.Linq;
 
 namespace WorkflowService1
 {
-    public class ExperimentalWorkflow : WorkflowService
+    public class ExperimentalWorkflow : Activity
     {
-        public ExperimentalWorkflow()
+        protected override Func<Activity> Implementation
         {
-            InitializeComponent();
-        }
-
-        private void InitializeComponent()
-        {
-            this.ConfigurationName = "ExperimentalWorkflow";
-
-            var requestVar = new Variable<int>() { Name = "request" };
-
-            this.Body = new Sequence()
+            get
             {
-                Variables =
-                {
-                  requestVar
-                },
-                Activities =
-                {
-                  new Receive()
-                  {
-                    DisplayName = "ExperimentalWorkflow_Receive",
-                    OperationName = "Receive",
-                    ServiceContractName =
-                      (XName)
-                      "{http://www.acme.com/}IExperimentalWorkflow",
-                    Action =
-                      "http://www.acme.com/IExperimentalWorkflow/Receive",
-                    CanCreateInstance = true,
-                    Content = new ReceiveParametersContent()
+                return () => {
+                    var requestVar = new Variable<int>()
                     {
-                      Parameters =
-                      {
-                        { "Request", new OutArgument<int>(requestVar) }
-                      }
-                    }
-                  },
-                  new SendReply()
-                  {
-                    DisplayName = "ExperimentalWorkflow_SendReply",
-                    Content = new SendParametersContent()
+                        Name = "request"
+                    };
+
+                    var receiveActivity = new Receive()
                     {
-                        Parameters =
+                        DisplayName = "ExperimentalWorkflow_Receive",
+                        OperationName = "Receive",
+                        ServiceContractName = (XName)"{http://www.acme.com/}IExperimentalWorkflow",
+                        Action = "http://www.acme.com/IExperimentalWorkflow/Receive",
+                        CanCreateInstance = true,
+                        Content = new ReceiveParametersContent()
                         {
-                            { "Response", new InArgument<int>(requestVar) }
+                            Parameters = {
+                                        {
+                                            "Request",
+                                            new OutArgument<int>(requestVar)
+                                        }
+                                    }
                         }
-                    }
-                  }
-                }
-            };
+                    };
+
+                    var sequence = new Sequence()
+                    {
+                        Variables = {
+                            requestVar
+                        },
+                        Activities = {
+                            receiveActivity,
+                            new SendReply() {
+                                DisplayName = "ExperimentalWorkflow_SendReply",
+                                Request = receiveActivity,
+                                Content = new SendParametersContent() {
+                                    Parameters = {
+                                        {
+                                            "Response",
+                                            new InArgument<int>(requestVar)
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    };
+
+                    return sequence;
+
+                };
+            }
+            set
+            {
+                base.Implementation = value;
+            }
         }
     }
 }
